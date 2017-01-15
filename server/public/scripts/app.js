5205b3e401efb61e7885c73fad554df81542a6e6
Ahkillah Davis <ahkillahdavis@gmail.com>
2017-01-15 15:26:03 -0600

    modified

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,14 +1,46 @@
-
-
+var clickIndex = 0;
 $(document).ready(function(){
-  $("#previous").on("click",function(){
+  $('#prev').on("click", function() {
     console.log("previous button click");
+    if(currentIndex > 0) {
+             clickIndex += 1;
+         }
+
+    addStudent(currentIndex);
   });
+  $('#next').on("click",function() {
+    console.log("next button click");
+    if(currentIndex <16){
+
+
+      clickIndex-= 1;
+
+      addStudent(currentIndex);
+  }
+
+  });
+
+
+  function getStudent(){
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
         console.log(data);
-      }
-    });
+        upsilon.json = data;
+        data.forEach(function(student){
+        appendDOM(student);
+
+      });
+    }
 });
+
+
+}
+function addStudent(i) {
+  var $studentDiv = $('<div class ="carousel"></div>');
+  $studentDiv.append('<p>' +data.name+ '</p>');
+  $studentDiv.append('<p>' +data.githubUserName+ '</p>');
+  $studentDiv.append('<p>' +data.shoutout+ '</p>');
+  $(".studentContainer").append($studentDiv);
+}
