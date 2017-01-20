db3c6f10d50bbca24187f4d10a9307aa7068373e
Ahkillah Davis <ahkillahdavis@gmail.com>
2017-01-19 18:27:35 -0600

    first commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,48 +1,67 @@
 var currentStudentIndex = 0;
 var showStudent = 0;
-
+var upsilon = [];
 $(document).ready(function(){
   $(".carousel").on("click", studentCarousel);
-  $('#prev').on("click", getPrevStudent);
-  $('#next').on("click", getNextStudent);
-  var interval = setInterval(getNextStudent, 10000);
+  $('#prev').on("click", getStudent);
+  $('#next').on("click", getStudent);
+  var interval = setInterval(getStudent, 10000);
+
+  console.log(upsilon);
+  // console.log(showStudent);
 });
 
-function getNextStudent(){
+function getStudent(){
   $.ajax({
     type: "GET",
     url: "/data",
     success: function(data){
+      upsilon = data.students;
       $("#studentContainer").empty();
       currentStudentIndex+=1;
-      if(currentStudentIndex <= data.students.length){
-        showStudent = data.students[currentStudentIndex];
-        addStudent(showStudent);
+      if(currentStudentIndex <= upsilon.length){
+        // showStudent = data.students[currentStudentIndex];
+        // showStudent = data.students;
+        // upsilon = showStudent;
+        console.log(upsilon);
+        addStudent(upsilon);
       }else{
         currentStudentIndex = 0;
-        showStudent = data.students[currentStudentIndex];
+        // showStudent = data.students[currentStudentIndex];
       }
-    }
-  });
-}
+      // $("#studentContainer").empty();
+      // // var index = data[currentIndex];
+      // console.log(data);
 
-function getPrevStudent(){
-  $.ajax({
-    type: "GET",
-    url: "/data",
-    success: function(data){
-      $("#studentContainer").empty();
-      // var index = data[currentIndex];
-      console.log(data);
-      currentStudentIndex -=1;
 
       if(currentStudentIndex >= 0){
-        showStudent = data.students[currentStudentIndex];
-        addStudent(showStudent);
+        // showStudent = data.students[currentStudentIndex];
+        currentStudentIndex -=1;
+        addStudent(upsilon);
       }
     }
   });
+
 }
+
+
+// function getPrevStudent(){
+//   $.ajax({
+//     type: "GET",
+//     url: "/data",
+//     success: function(data){
+      // $("#studentContainer").empty();
+      // // var index = data[currentIndex];
+      // console.log(data);
+      // currentStudentIndex -=1;
+      //
+      // if(currentStudentIndex >= 0){
+      //   showStudent = data.students[currentStudentIndex];
+      //   addStudent(showStudent);
+      // }
+  //   }
+  // });
+// }
 function studentCarousel(){
   currentStudentIndex = $(this).attr('id');
   $.ajax({
@@ -57,17 +76,18 @@ function studentCarousel(){
 );
 }
 function addStudent(person) {
+  console.log(person[currentStudentIndex]);
   $(".person").fadeOut(1000);
   var $studentDiv = $('<div class ="person"></div>');
-  $studentDiv.append('<p><mark>' + person.name + '</mark></p>');
-  $studentDiv.append('<p>' +person.githubUserName+'</p>');
-  $studentDiv.append('<p>' +person.shoutout+'</p>');
+  $studentDiv.append('<p><mark>' + person[currentStudentIndex].name + '</mark></p>');
+  $studentDiv.append('<p>' +person[currentStudentIndex].githubUserName+'</p>');
+  $studentDiv.append('<p>' +person[currentStudentIndex].shoutout+'</p>');
   $("#studentContainer").append($studentDiv);
   $($studentDiv).hide().appendTo("#studentContainer").fadeIn(2000);
 }
 
-function clock() {
-  $("#clock").html = new Date().toLocaleTimeString();
-  $("#prev").on("click",clearInterval(interval));
-  $("#next").on("click",clearInterval(interval));
-}
+// function clock() {
+//   $("#clock").html = new Date().toLocaleTimeString();
+//   $("#prev").on("click",clearInterval(interval));
+//   $("#next").on("click",clearInterval(interval));
+// }
