6314afc9a695f2cbec6d7e06520c7fc9aec5e2cc
Ahkillah Davis <ahkillahdavis@gmail.com>
2017-02-01 13:49:17 -0600

    modified

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 This challenge will require you to setup and run the application in this repository.
 See the assignment directions if you need a refresher on how to run this.
+
+Challenge Overview
+I created a new JSON data file and inside it you will find an array of objects. Each object, is each one of you!
+
+AJAX Request
+
+Your first task is to make an AJAX call from the client side app.js using the .ajax method. The AJAX call will be a GET request that accesses the /data URL. Upon success, it should bring the data back down.
+
+On the DOM
+
+On the DOM should be:
+
+One person's information
+A series of 17 (or the number of people in the data array) index points with the first person's index highlighted or called out in style differently than the others.
+A 'Next' button and a 'Previous' button
+Clicking on the Next button should navigate to the next person, clicking on the Previous button should navigate to the previous person. The highlighted index point should update also as you click through to other people.
+Person Display
+
+When a person is displayed, show their name, their Github link, and their piece of shoutout feedback. Only one person should be showcased at any given time.
+
+You will need to combine everything you learned this week to accomplish this task, and each of the challenges you have completed this week play a part in this task.
+
+Working Example
+
+Here is a similar example from Zeta so you can see the functionality. It's really ugly, however. The code is also minified (no cheating!):
+
+https://polar-ravine-37299.herokuapp.com/
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,93 +1,83 @@
 var currentStudentIndex = 0;
-var showStudent = 0;
 var upsilon = [];
+var time = 0;
 $(document).ready(function(){
-  $(".carousel").on("click", studentCarousel);
-  $('#prev').on("click", getStudent);
-  $('#next').on("click", getStudent);
-  var interval = setInterval(getStudent, 10000);
+  $('#prev').on("click", getPrevStudent);
+  $('#next').on("click", getNextStudent);
+  var interval = setInterval(getNextStudent, 10000);
+  getStudent();
 
-  console.log(upsilon);
-  // console.log(showStudent);
-});
-
-function getStudent(){
-  $.ajax({
-    type: "GET",
-    url: "/data",
-    success: function(data){
-      upsilon = data.students;
-      $("#studentContainer").empty();
-      currentStudentIndex+=1;
-      if(currentStudentIndex <= upsilon.length){
-        // showStudent = data.students[currentStudentIndex];
-        // showStudent = data.students;
-        // upsilon = showStudent;
-        console.log(upsilon);
-        addStudent(upsilon);
-      }else{
-        currentStudentIndex = 0;
-        // showStudent = data.students[currentStudentIndex];
+  function getStudent(){
+    $.ajax({
+      type: "GET",
+      url: "/data",
+      success: function(data){
+        upsilon = data.students;
+        makeTracker();
+        addStudent();
       }
-      // $("#studentContainer").empty();
-      // // var index = data[currentIndex];
-      // console.log(data);
-
-
-      if(currentStudentIndex >= 0){
-        // showStudent = data.students[currentStudentIndex];
-        currentStudentIndex -=1;
-        addStudent(upsilon);
-      }
-    }
-  });
-
-}
-
-
-// function getPrevStudent(){
-//   $.ajax({
-//     type: "GET",
-//     url: "/data",
-//     success: function(data){
-      // $("#studentContainer").empty();
-      // // var index = data[currentIndex];
-      // console.log(data);
-      // currentStudentIndex -=1;
-      //
-      // if(currentStudentIndex >= 0){
-      //   showStudent = data.students[currentStudentIndex];
-      //   addStudent(showStudent);
-      // }
-  //   }
-  // });
-// }
-function studentCarousel(){
-  currentStudentIndex = $(this).attr('id');
-  $.ajax({
-    type: "GET",
-    url: "/data",
-    success: function(data){
-      $("#studentContainer").empty();
-      showStudent = data.students[currentStudentIndex];
-      addStudent(showStudent);
-    }
+    });
   }
-);
-}
-function addStudent(person) {
-  console.log(person[currentStudentIndex]);
-  $(".person").fadeOut(1000);
-  var $studentDiv = $('<div class ="person"></div>');
-  $studentDiv.append('<p><mark>' + person[currentStudentIndex].name + '</mark></p>');
-  $studentDiv.append('<p>' +person[currentStudentIndex].githubUserName+'</p>');
-  $studentDiv.append('<p>' +person[currentStudentIndex].shoutout+'</p>');
-  $("#studentContainer").append($studentDiv);
-  $($studentDiv).hide().appendTo("#studentContainer").fadeIn(2000);
-}
 
-// function clock() {
-//   $("#clock").html = new Date().toLocaleTimeString();
-//   $("#prev").on("click",clearInterval(interval));
-//   $("#next").on("click",clearInterval(interval));
-// }
+  function makeTracker() {
+
+    for (var i = 0; i < upsilon.length; i++){
+      var indexToDisplay = i +1;
+      $("#trackStudent").append('<div class = "newStudent" id = "student-' + indexToDisplay +'">' + indexToDisplay + '</div>');
+    }
+    updateStudent();
+  }
+
+  function getNextStudent(){
+
+    $("#studentContainer").empty();
+    currentStudentIndex++;
+    if(currentStudentIndex >= upsilon.length){
+      currentStudentIndex = 0;
+    }
+    updateStudent();
+    addStudent();
+    changeTime();
+  }
+
+  function getPrevStudent(){
+    $("#studentContainer").empty();
+    currentStudentIndex --;
+
+    if(currentStudentIndex < 0){
+      currentStudentIndex = upsilon.length - 1;
+    }
+
+    addStudent();
+    updateStudent();
+    changeTime();
+  }
+
+
+  function addStudent() {
+    var student = upsilon[currentStudentIndex];
+    $(".person").fadeOut(1000);
+    var $studentDiv = $('<div class ="person"></div>');
+    $studentDiv.append('<p><mark>' + student.name + '</mark></p>');
+    $studentDiv.append('<p>' +student.githubUserName+'</p>');
+    $studentDiv.append('<p>' +student.shoutout+'</p>');
+
+    $("#studentContainer").append($studentDiv);
+    $($studentDiv).hide().appendTo("#studentContainer").fadeIn(2000);
+  }
+
+  function updateStudent() {
+
+    $("#trackStudent").children().each(function(i, item) {
+
+      if(currentStudentIndex == i) {
+        $(this).addClass("highlightStudent");
+      } else {
+        $(this).removeClass("highlightStudent");
+      }
+    });
+  }
+  function changeTime(){
+    clearInterval(time);
+  }
+});
